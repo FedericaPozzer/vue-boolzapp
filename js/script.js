402b90f9d3991ac377d6b437211ce1f3d0727c85
Federica Pozzer <federica.pozzer@gmail.com>
2023-02-15 14:58:58 +0100

    O.o

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@
 // MILESTONE 2
     // * Visualizzazione dinamica dei messaggi: tramite la direttiva v -for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
     // * Click sul contatto mostra la conversazione del contatto cliccato
+        // ? ok!
 
 // MILESTONE 3
     // * Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
